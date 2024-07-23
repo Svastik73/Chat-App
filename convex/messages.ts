da361577bc85390c1a826aac304e5a8fa47fa2b4
Svastik <kanwar7svastik@gmail.com>
2024-07-23 19:11:11 +0530

    your commit message

---
+++ convex/messages.ts
@@ -0,0 +1,41 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+// send recieve meesages
+export const sendTextMessage=mutation({
+    args:{
+        sender: v.string(),
+        content: v.string(),
+        conversation: v.id("conversations"),
+        
+    },
+    handler: async(ctx,args)=>{
+        const identity=await ctx.auth.getUserIdentity();
+        
+        if(!identity)  throw new ConvexError("Not uthenticated");    
+        
+        const user=await ctx.db.query("users")
+            .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", identity.tokenIdentifier))
+            .unique();
+        
+        if(!user) throw new ConvexError("User not found");
+        
+        const conversation=await ctx.db.query("conversations")
+        .filter(q=>q.eq(q.field("_id"),args.conversation)).first();
+
+        if(!conversation) throw new ConvexError("No conversation found");
+
+        if(!conversation.participants.includes(user._id)){
+            throw new ConvexError("You are not part of this conversation");
+        }
+        
+        await ctx.db.insert("messages",{
+            sender:args.sender,
+            content:args.content,
+            conversation:args.conversation,
+            messageType:"text",
+        });
+
+        //TODO: CHATGPT SUPPORT
+
+    },
+});
