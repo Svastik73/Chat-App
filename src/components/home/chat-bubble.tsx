0ffb58a09c99669633f5b82a0957903db2cca379
Svastik <kanwar7svastik@gmail.com>
2024-07-25 23:38:55 +0530

    group actions wrapped up

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -6,26 +6,31 @@ import Image from "next/image";
 import { useState } from "react";
 import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
 import ReactPlayer from "react-player";
-type ChatBubbleProps={
-    message:IMessage;
-	me:any;
-	previousMessage?:IMessage;
-}
+import ChatAvatarActions from "./chat-avatar-actions";
+import { Bot } from "lucide-react";
 
+type ChatBubbleProps = {
+	message: IMessage;
+	me: any;
+	previousMessage?: IMessage;
+};
 
-const ChatBubble = ({me,message,previousMessage}:ChatBubbleProps) => {
-	const date=new Date(message._creationTime);
-	const hour=date.getHours().toString().padStart(2,"0");
-	const minute=date.getMinutes().toString().padStart(2,"0");
-	const time=`${hour}:${minute}`;
+const ChatBubble = ({ me, message, previousMessage }: ChatBubbleProps) => {
+	const date = new Date(message._creationTime);
+	const hour = date.getHours().toString().padStart(2, "0");
+	const minute = date.getMinutes().toString().padStart(2, "0");
+	const time = `${hour}:${minute}`;
 
-	const{selectedConversation}=useConversationStore();
-    const isMember=selectedConversation?.participants.includes(message.sender._id)||false;
-	const isGroup=selectedConversation?.isGroup;
-	const fromMe=message.sender._id==me._id;
-	const bgClass=fromMe?"bg-green-chat":"bg-white dark:bg-gray-primary";
-	const [open,setOpen]=useState(false);// whether image dialog should open or not!
-	
+	const { selectedConversation } = useConversationStore();
+	const isMember = selectedConversation?.participants.includes(message.sender?._id) || false;
+	const isGroup = selectedConversation?.isGroup;
+	const fromMe = message.sender?._id === me._id;
+	const fromAI = message.sender?.name === "ChatGPT";
+	const bgClass = fromMe ? "bg-green-chat" : !fromAI ? "bg-white dark:bg-gray-primary" : "bg-blue-500 text-white";
+
+	console.log(message.sender);
+	const [open, setOpen] = useState(false);
+
 	const renderMessageContent = () => {
 		switch (message.messageType) {
 			case "text":
@@ -38,64 +43,48 @@ const ChatBubble = ({me,message,previousMessage}:ChatBubbleProps) => {
 				return null;
 		}
 	};
-	
-	if(!fromMe){
-		return(
+
+	if (!fromMe) {
+		return (
 			<>
-			<DateIndicator message={message} previousMessage={previousMessage}/>
-			  <div className="flex gap-1 w-2/3">
-			     <ChatBubbleAvatar message={message} isMember={isMember} isGroup={isGroup}/>
-				  <div  className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
-                       <OtherMessageIndicator></OtherMessageIndicator> {/*This component is to show triangle comming out from chat */}
-				       
-                        {renderMessageContent()}
-						 
-						{open && <ImageDialog
-						   src={message.content}
-						   open={open}
-						   onClose={()=>setOpen(false)}
-						/>}
-						
-						<MessageTime time={time}
-						fromMe={fromMe}>
-
-						</MessageTime>
-				  </div>
-			  </div>
+				<DateIndicator message={message} previousMessage={previousMessage} />
+				<div className='flex gap-1 w-2/3'>
+					<ChatBubbleAvatar isGroup={isGroup} isMember={isMember} message={message}  />
+					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
+						{!fromAI && <OtherMessageIndicator />}
+						{fromAI && <Bot size={16} className='absolute bottom-[2px] left-2' />}
+						{<ChatAvatarActions message={message} me={me} />}
+						{renderMessageContent()}
+						{open && <ImageDialog src={message.content} open={open} onClose={() => setOpen(false)} />}
+						<MessageTime time={time} fromMe={fromMe} />
+					</div>
+				</div>
 			</>
-		)
+		);
 	}
-	return (<>
-	<DateIndicator message={message} previousMessage={previousMessage}/>
-	 <div className="flex gap-1 w-2/3 ml-auto">
-			      <div  className={`flex z-20 max-w-fit px-2 pt-1 rounded-md shadow-md ml-auto relative ${bgClass}`}>
-					<SelfMessageIndicator></SelfMessageIndicator>
-				 {/*This component is to show triangle comming out from chat */}
-			     
 
-				 {renderMessageContent()}
-				 {open && <ImageDialog
-						   src={message.content}
-						   open={open}
-						   onClose={()=>setOpen(false)}
-						/>}	
-					
-						<MessageTime time={time}
-						fromMe={fromMe} >
+	return (
+		<>
+			<DateIndicator message={message} previousMessage={previousMessage} />
 
-						</MessageTime>
-				  </div>
-			  </div>
-	</>)
-
+			<div className='flex gap-1 w-2/3 ml-auto'>
+				<div className={`flex  z-20 max-w-fit px-2 pt-1 rounded-md shadow-md ml-auto relative ${bgClass}`}>
+					<SelfMessageIndicator />
+					{renderMessageContent()}
+					{open && <ImageDialog src={message.content} open={open} onClose={() => setOpen(false)} />}
+					<MessageTime time={time} fromMe={fromMe} />
+				</div>
+			</div>
+		</>
+	);
 };
 export default ChatBubble;
-const VideoMessage=({message}:{message:IMessage})=>{
-	return <ReactPlayer url={message.content}  width='250px' height='250px' controls={true}/>
-}
 
+const VideoMessage = ({ message }: { message: IMessage }) => {
+	return <ReactPlayer url={message.content} width='250px' height='250px' controls={true} light={true} />;
+};
 
-const ImageMessage = ({ message ,handleClick}: { message: IMessage,handleClick:()=>void }) => {
+const ImageMessage = ({ message, handleClick }: { message: IMessage; handleClick: () => void }) => {
 	return (
 		<div className='w-[250px] h-[250px] m-2 relative'>
 			<Image
@@ -103,15 +92,14 @@ const ImageMessage = ({ message ,handleClick}: { message: IMessage,handleClick:(
 				fill
 				className='cursor-pointer object-cover rounded'
 				alt='image'
-		         onClick={handleClick}
+				onClick={handleClick}
 			/>
 		</div>
 	);
 };
 
-
 const ImageDialog = ({ src, onClose, open }: { open: boolean; src: string; onClose: () => void }) => {
-	return ( // show image on clicking in new dialog box
+	return (
 		<Dialog
 			open={open}
 			onOpenChange={(isOpen) => {
@@ -127,7 +115,6 @@ const ImageDialog = ({ src, onClose, open }: { open: boolean; src: string; onClo
 	);
 };
 
-
 const MessageTime = ({ time, fromMe }: { time: string; fromMe: boolean }) => {
 	return (
 		<p className='text-[10px] mt-2 self-end flex gap-1 items-center'>
@@ -135,15 +122,14 @@ const MessageTime = ({ time, fromMe }: { time: string; fromMe: boolean }) => {
 		</p>
 	);
 };
-const SelfMessageIndicator = () => (
-	<div className='absolute bg-green-chat top-0 -right-[3px] w-3 h-3 rounded-br-full overflow-hidden' />
-  );   // quarter circle to right
-  
- 
+
 const OtherMessageIndicator = () => (
 	<div className='absolute bg-white dark:bg-gray-primary top-0 -left-[4px] w-3 h-3 rounded-bl-full' />
-); // quarter circle to left
+);
 
+const SelfMessageIndicator = () => (
+	<div className='absolute bg-green-chat top-0 -right-[3px] w-3 h-3 rounded-br-full overflow-hidden' />
+);
 
 const TextMessage = ({ message }: { message: IMessage }) => {
 	const isLink = /^(ftp|http|https):\/\/[^ "]+$/.test(message.content); // Check if the content is a URL
@@ -155,7 +141,7 @@ const TextMessage = ({ message }: { message: IMessage }) => {
 					href={message.content}
 					target='_blank'
 					rel='noopener noreferrer'
-					className={`mr-2 text-sm font-light text-green-400 underline`}
+					className={`mr-2 text-sm font-light text-blue-400 underline`}
 				>
 					{message.content}
 				</a>
